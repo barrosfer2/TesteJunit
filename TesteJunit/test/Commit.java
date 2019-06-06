540eb905d1423a36a3e44a97ea53d3ce4806694c
GC-Aula <GC-Aula@GC-Aula-PC>
2019-06-06 20:53:22 -0300

    Fernando Barros Commit2

--- TesteJunit/test/Commit.java
+++ TesteJunit/test/Commit.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author GC-Aula
- */
 public class Commit {
     /* Aula GC "02/05/2019" mais na verdade hoje é dia 06/06/2019 */
+    System.out.println ("Alo teste");
 }
+
--- TesteJunit/test/NewClassTest.java
+++ TesteJunit/test/NewClassTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.*;
 public class NewClassTest {
     /* Aula GC "02/05/2019" mais na verdade hoje é dia 06/06/2019 */
     public NewClassTest() {
+        println "Aloo";
     }
     
     @BeforeClass
